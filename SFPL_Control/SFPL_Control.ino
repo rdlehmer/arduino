c23a42b61edd13940b221f9974e32f1dc5e29101
DESKTOP-KDBVAJP\rdleh <rdlehmer@yahoo.com>
2023-09-28 00:38:45 -0700

    4 channel prototype - needs real buttons and timings

--- SFPL_Control/SFPL_Control.ino
+++ SFPL_Control/SFPL_Control.ino
@@ -2,14 +2,10 @@
 // SFPL Control V0.1
 // Ron Lehmer   2023-09-27
 //
-// For the Arduino Uno R3/2560 Mega
-//
-//  Updated for 4x4 keypad testing
+// For the Arduino Uno R3
 //
 // Package/library dependencies for Arduino Library manager
 //
-// PCF8574 Library (not "PCF8574")
-// LiquidCrystal I2C
 //
 //
 
@@ -63,6 +59,14 @@ class SFPL_Timer {
     }
     Serial.println(inhibit);
   }
+
+  void set_on_time( long arg_on_time ) {
+    on_time = arg_on_time;
+  }
+
+  void set_off_time( long arg_off_time ) {
+    off_time = arg_off_time;
+  }
   
   int state;
   int inhibit;
@@ -73,15 +77,38 @@ class SFPL_Timer {
   unsigned long timer_start;
   
 };
-
+#if 0
 SFPL_Timer timer1(0,15000,30000);
+SFPL_Timer timer2(0,15000,15000);
+SFPL_Timer timer3(0,60000,60000);
+SFPL_Timer timer4(0,60000,60000);
+#endif
+SFPL_Timer timers[4];
+
 //
 // Library Include Files
 //
 
 
 void setup() {
+  
   initPins();
+
+//  timers[0] = new SFPL_Timer(0,15000,30000);
+//  timers[1] = new SFPL_Timer(0,15000,15000);
+//  timers[2] = new SFPL_Timer(0,60000,60000);
+//  timers[3] = new SFPL_Timer(0,60000,60000);
+
+  timers[0].set_on_time(15000);
+  timers[0].set_off_time(30000);
+  timers[1].set_on_time(15000);
+  timers[1].set_off_time(15000);
+  timers[2].set_on_time(60000);
+  timers[2].set_off_time(60000);
+  timers[3].set_on_time(60000);
+  timers[3].set_off_time(60000);
+  
+  
   delay(3000);
   Serial.begin(9600);
 //  timer1.set();
@@ -89,18 +116,29 @@ void setup() {
 
 
 void loop() {
+  int button[4];
+  int i;
+  for ( i = 0 ; i < 4 ; i++ ) {
+    button[i] = digitalRead(8+i);
+    if ( button[i] == 0 ) {
+      timers[i].set();
+    }
+    if ( timers[i].state == 1 ) {
+      digitalWrite(4+i, HIGH);
+    }
+    else {
+      digitalWrite(4+i, LOW);
+    }
+    timers[i].update();
+  }
+#if 0
   int button1;
   button1 = digitalRead(2);
+
   if ( button1 == 0 ) {
     timer1.set();
   }
-#if 0  
-  while ( timer1.state == 1 ) {
-    digitalWrite(4, HIGH);
-    timer1.update();
-  }
-  digitalWrite(4, LOW);
-#endif
+
   if ( timer1.state == 1 ) {
     digitalWrite(4, HIGH);
   }
@@ -108,6 +146,7 @@ void loop() {
     digitalWrite(4, LOW);
   }
   timer1.update();
+#endif
 }
 
 void initPins() {
@@ -121,10 +160,10 @@ void initPins() {
   digitalWrite(6, LOW);
   pinMode(7, OUTPUT);
   digitalWrite(7, LOW);
-  pinMode(8, INPUT);
-  pinMode(9, INPUT);
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
   pinMode(12, INPUT);
   pinMode(13, INPUT);
   
