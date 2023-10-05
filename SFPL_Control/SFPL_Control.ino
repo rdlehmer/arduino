0271b47bc7d36bf52ea3e209f8449280772455dd
DESKTOP-KDBVAJP\rdleh <rdlehmer@yahoo.com>
2023-10-05 09:56:07 -0700

    Updated for delay and one button control

--- SFPL_Control/SFPL_Control.ino
+++ SFPL_Control/SFPL_Control.ino
@@ -12,14 +12,14 @@
 class SFPL_Timer {
  public:
   SFPL_Timer() {
-    state = 0;
+    _state = 0;
     inhibit = 0;
     timer_start = millis();
     timer_milli = timer_start;
   }
   
-  SFPL_Timer(int arg_state, int arg_on_time, int arg_off_time) {
-    state = arg_state;
+  SFPL_Timer(long arg_state, long arg_on_time, long arg_off_time) {
+    _state = arg_state;
     on_time = arg_on_time;
     off_time = arg_off_time;
     timer_start = millis();
@@ -30,31 +30,37 @@ class SFPL_Timer {
   }
   
   void set() {
-    if ( ( state != 1 ) && ( inhibit == 0 ) ) {
+    if ( ( _state == 0 ) && ( inhibit == 0 ) ) {
       timer_start = millis();      
-      state = 1;
+      if ( delay_time > 0 ) 
+        _state = -1;
+      else
+        _state = 1;
       inhibit = 1;
     } 
   }
   
   void reset() {
-    state = 0;
+    _state = 0;
   }
   
   void update() {
     unsigned long timer_milli = millis();
     unsigned long differ_milli = timer_milli - timer_start;
-    if (( state == 1 ) && ( differ_milli > on_time )) {
-      state = 0;
+    if (( _state == -1 ) && ( differ_milli > delay_time ) ) {
+      _state = 1;
+    }
+    if (( _state == 1 ) && ( differ_milli > ( delay_time + on_time ) )) {
+      _state = 0;
     }
     Serial.print(timer_milli);
     Serial.print(" ");
     Serial.print(timer_start);
     Serial.print(" ");
-    Serial.print(state);
+    Serial.print(_state);
     Serial.print(" ");
 
-    if (( inhibit == 1 ) && ( differ_milli > ( on_time + off_time )) ) {
+    if (( inhibit == 1 ) && ( differ_milli > ( delay_time + on_time + off_time )) ) {
       inhibit = 0;
     }
     Serial.println(inhibit);
@@ -68,22 +74,30 @@ class SFPL_Timer {
     off_time = arg_off_time;
   }
   
-  int state;
+  void set_delay_time( long arg_delay_time ) {
+    delay_time = arg_delay_time;
+  }
+
+  int state() {
+    int tmp;
+    if ( _state == 1 )
+      tmp = 1;
+    else
+      tmp = 0;
+    return(tmp);
+  }
+  int _state;
   int inhibit;
  private:
   long on_time;
   long off_time;
+  long delay_time;
   unsigned long timer_milli;
   unsigned long timer_start;
   
 };
-#if 0
-SFPL_Timer timer1(0,15000,30000);
-SFPL_Timer timer2(0,15000,15000);
-SFPL_Timer timer3(0,60000,60000);
-SFPL_Timer timer4(0,60000,60000);
-#endif
-SFPL_Timer timers[4];
+
+SFPL_Timer timers[8];
 
 //
 // Library Include Files
@@ -99,15 +113,18 @@ void setup() {
 //  timers[2] = new SFPL_Timer(0,60000,60000);
 //  timers[3] = new SFPL_Timer(0,60000,60000);
 
-  timers[0].set_on_time(15000);
-  timers[0].set_off_time(30000);
-  timers[1].set_on_time(15000);
-  timers[1].set_off_time(15000);
+  timers[0].set_on_time(60000);
+  timers[0].set_off_time(60000);
+  timers[0].set_delay_time(0);
+  timers[1].set_on_time(60000);
+  timers[1].set_off_time(60000);
+  timers[1].set_delay_time(1000);
   timers[2].set_on_time(60000);
   timers[2].set_off_time(60000);
+  timers[2].set_delay_time(2000);
   timers[3].set_on_time(60000);
   timers[3].set_off_time(60000);
-  
+  timers[3].set_delay_time(3000);
   
   delay(3000);
   Serial.begin(9600);
@@ -120,33 +137,21 @@ void loop() {
   int i;
   for ( i = 0 ; i < 4 ; i++ ) {
     button[i] = digitalRead(8+i);
-    if ( button[i] == 0 ) {
+
+//    if ( button[i] == 0 ) {     // 
+    if ( button[0] == 0 ) {
       timers[i].set();
     }
-    if ( timers[i].state == 1 ) {
+    if ( timers[i].state() == 1 ) {
       digitalWrite(4+i, HIGH);
     }
     else {
       digitalWrite(4+i, LOW);
     }
+    Serial.print(i);
+    Serial.print(" ");
     timers[i].update();
   }
-#if 0
-  int button1;
-  button1 = digitalRead(2);
-
-  if ( button1 == 0 ) {
-    timer1.set();
-  }
-
-  if ( timer1.state == 1 ) {
-    digitalWrite(4, HIGH);
-  }
-  else {
-    digitalWrite(4, LOW);
-  }
-  timer1.update();
-#endif
 }
 
 void initPins() {
